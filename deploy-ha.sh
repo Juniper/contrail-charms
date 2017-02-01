c2bed9e91a47d1cf7b4bc38aa351b8d84a932bbf
sgurumurthy <sgurumurthy@juniper.net>
2017-02-01 16:50:13 +0000

    Support for xenial deployment in bundle

--- deploy-ha.sh
+++ deploy-ha.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 juju deploy contrail-docker-bundle-ha.yaml
-juju attach contrail-control contrail-control="/home/ubuntu/contrail-controller-u14.04-4.0.0.0-3031.tar.gz"
-juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-u14.04-4.0.0.0-3031.tar.gz"
-juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-u14.04-4.0.0.0-3031.tar.gz"
-juju attach contrail-lb contrail-lb="/home/ubuntu/contrail-lb-u14.04-4.0.0.0-3031.tar.gz"
+juju attach contrail-control contrail-control="/home/ubuntu/contrail-controller-u16.04-4.0.0.0-3034.tar.gz"
+juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-u16.04-4.0.0.0-3034.tar.gz"
+juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-u16.04-4.0.0.0-3034.tar.gz"
+juju attach contrail-lb contrail-lb="/home/ubuntu/contrail-lb-u16.04-4.0.0.0-3034.tar.gz"
