6b2f15f0a1d79b97a87706148df0f5f7d37643a9
Andrey Pavlov <andrey.mp@gmail.com>
2017-04-24 22:02:51 +0300

    remove outdated bundles, rework current bundles

--- deploy-ha.sh
+++ deploy-ha.sh
@@ -3,4 +3,3 @@ juju deploy contrail-docker-bundle-ha.yaml
 juju attach contrail-control contrail-control="/home/ubuntu/contrail-controller-u16.04-4.0.0.0-3034.tar.gz"
 juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-u16.04-4.0.0.0-3034.tar.gz"
 juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-u16.04-4.0.0.0-3034.tar.gz"
-juju attach contrail-lb contrail-lb="/home/ubuntu/contrail-lb-u16.04-4.0.0.0-3034.tar.gz"
