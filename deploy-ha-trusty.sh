7a3cd62125626b1f81045d0e44c6c1c591131403
Andrey Pavlov <andrey.mp@gmail.com>
2017-05-03 11:26:19 +0300

    change paths in deploy scripts/bundles

--- deploy-ha-trusty.sh
+++ deploy-ha-trusty.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
-juju deploy contrail-docker-bundle-ha-trusty.yaml
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+juju deploy "$my_dir/contrail-docker-bundle-ha-trusty.yaml"
 juju attach contrail-control contrail-control="/home/ubuntu/contrail-controller-u14.04-4.0.0.0-3038.tar.gz"
 juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-u14.04-4.0.0.0-3038.tar.gz"
 juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-u14.04-4.0.0.0-3038.tar.gz"
--- deploy-ha.sh
+++ deploy-ha.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
-juju deploy contrail-docker-bundle-ha.yaml
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+juju deploy "$my_dir/contrail-docker-bundle-ha.yaml"
 juju attach contrail-control contrail-control="/home/ubuntu/contrail-controller-u16.04-4.0.0.0-3034.tar.gz"
 juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-u16.04-4.0.0.0-3034.tar.gz"
 juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-u16.04-4.0.0.0-3034.tar.gz"
