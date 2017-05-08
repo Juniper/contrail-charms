5b72b3edaddfba2718dc1dffff31421e70cfa54b
Andrey Pavlov <andrey.mp@gmail.com>
2017-05-08 22:21:58 +0300

    update scripts to new charm names

--- deploy-ha-trusty.sh
+++ deploy-ha-trusty.sh
@@ -2,6 +2,8 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 juju deploy "$my_dir/contrail-docker-bundle-ha-trusty.yaml"
-juju attach contrail-control contrail-control="/home/ubuntu/contrail-controller-u14.04-4.0.0.0-3038.tar.gz"
-juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-u14.04-4.0.0.0-3038.tar.gz"
-juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-u14.04-4.0.0.0-3038.tar.gz"
+
+BUILD="3065"
+juju attach contrail-controller contrail-controller="/home/ubuntu/contrail-controller-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
+juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
+juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
--- deploy-ha.sh
+++ deploy-ha.sh
@@ -2,6 +2,8 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 juju deploy "$my_dir/contrail-docker-bundle-ha.yaml"
-juju attach contrail-control contrail-control="/home/ubuntu/contrail-controller-u16.04-4.0.0.0-3034.tar.gz"
-juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-u16.04-4.0.0.0-3034.tar.gz"
-juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-u16.04-4.0.0.0-3034.tar.gz"
+
+BUILD="3065"
+juju attach contrail-controller contrail-controller="/home/ubuntu/contrail-controller-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
+juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
+juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
