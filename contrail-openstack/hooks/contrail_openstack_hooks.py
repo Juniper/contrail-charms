9fe183e3b73ad25db27910456899806ce47ab721
Andrey Pavlov <andrey.mp@gmail.com>
2017-09-15 09:06:41 +0300

    pass empty dict in list as extra_middleware to neutron
    it doesn't allow to pass empty list

--- contrail-openstack/hooks/contrail_openstack_hooks.py
+++ contrail-openstack/hooks/contrail_openstack_hooks.py
@@ -252,7 +252,7 @@ def neutron_api_joined(rel_id=None):
             }
         }]
     else:
-        settings["extra_middleware"] = []
+        settings["extra_middleware"] = [{}]
     relation_set(relation_id=rel_id, relation_settings=settings)
 
     # we need to update api-paste.ini cause old versions of neutron-api charm
