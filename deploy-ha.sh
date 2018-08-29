c728ebc15785fb661ef1931bbdc4f9a37eb2d23d
Andrey Pavlov <andrey.mp@gmail.com>
2018-08-29 22:49:48 +0300

    update example of the bundle for R5

--- deploy-ha.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-my_file="$(readlink -e "$0")"
-my_dir="$(dirname $my_file)"
-juju deploy "$my_dir/contrail-docker-bundle-ha.yaml"
-
-BUILD="3065"
-juju attach contrail-controller contrail-controller="/home/ubuntu/contrail-controller-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
-juju attach contrail-analytics contrail-analytics="/home/ubuntu/contrail-analytics-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
-juju attach contrail-analyticsdb contrail-analyticsdb="/home/ubuntu/contrail-analyticsdb-ubuntu14.04-4.0.0.0-$BUILD.tar.gz"
