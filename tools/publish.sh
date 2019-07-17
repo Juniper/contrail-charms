082b967118b7e45efb971be790f67509690c0de2
Andrey Pavlov <andrey.mp@gmail.com>
2019-07-17 14:48:17 +0300

    add new kubernetes charms to publish tool

--- tools/publish.sh
+++ tools/publish.sh
@@ -7,11 +7,12 @@ my_dir="$(dirname $my_file)"
 
 charm login
 team="juniper-os-software"
-for folder in contrail-agent contrail-analytics contrail-analyticsdb contrail-controller contrail-keystone-auth contrail-openstack ; do
+for folder in contrail-agent contrail-analytics contrail-analyticsdb contrail-controller contrail-keystone-auth contrail-openstack contrail-kubernetes-master contrail-kubernetes-node ; do
   id="~$team/$folder"
   res=`charm push "$my_dir/../$folder" cs:${id}`
   echo "$res"
   num=`echo "$res" | grep "cs:${id}" | sed "s|^.*cs:${id}-\([0-9]*\).*$|\1|"`
   charm release cs:${id}-$num
   charm grant "${id}" --channel stable --acl read --set everyone
+  charm set "${id}" bugs-url="https://github.com/Juniper/contrail-charms/issues" homepage="https://github.com/Juniper/contrail-charms"
 done
