05dd44489989ca53d0018ea5b174c17bfa8553f5
tikitavi <rtikitavi@gmail.com>
2019-10-08 16:08:28 +0300

    fix check_output in _get_iface_gateway_ip for Python3

--- contrail-agent/hooks/contrail_agent_utils.py
+++ contrail-agent/hooks/contrail_agent_utils.py
@@ -90,7 +90,7 @@ def _get_default_gateway_iface():
 
 def _get_iface_gateway_ip(iface):
     ifaces = [iface, "vhost0"]
-    for line in check_output(["route", "-n"]).splitlines()[2:]:
+    for line in check_output(["route", "-n"]).decode('UTF-8').splitlines()[2:]:
         l = line.split()
         if "G" in l[3] and l[7] in ifaces:
             log("Found gateway {} for interface {}".format(l[1], iface))
